168faac87165ae3092eec11d427263e0fff8e988
Mostafa Shaer <mostafa-alshaer@hotmail.com>
2020-07-25 00:33:25 +0300

    -- refactoring task entity.

--- repository/src/main/java/com/mostafa/springboot/ddd/task/repository/entity/TaskEntity.java
+++ repository/src/main/java/com/mostafa/springboot/ddd/task/repository/entity/TaskEntity.java
@@ -1,58 +1,18 @@
 package com.mostafa.springboot.ddd.task.repository.entity;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
 
 @Entity
 @Table(name = "task")
-public class TaskEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, name="created_at")
-    @CreationTimestamp
-    private Date createdAt;
-
-    @Column(nullable = false, name="updated_at")
-    @UpdateTimestamp
-    private Date updatedAt;
+public class TaskEntity extends BaseEntity{
 
     @NotBlank
-    String description;
+    private String description;
 
     @NotNull
-    Boolean completed;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+    private Boolean completed;
 
     public String getDescription() {
         return description;
