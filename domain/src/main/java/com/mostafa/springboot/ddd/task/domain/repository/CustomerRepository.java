f47dde045eb79290d22792eb11aafe59f9c917e9
Mostafa Shaer <mostafa-alshaer@hotmail.com>
2020-07-24 16:08:10 +0300

    -- removing unneeded classes.

--- domain/src/main/java/com/mostafa/springboot/ddd/task/domain/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mostafa.springboot.ddd.task.domain.repository;
-
-import com.mostafa.springboot.ddd.task.domain.aggregate.Customer;
-
-public interface CustomerRepository {
-
-    Customer getCustomerById(Integer customerId);
-}
--- domain/src/main/java/com/mostafa/springboot/ddd/task/domain/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.mostafa.springboot.ddd.task.domain.service;
-
-import com.mostafa.springboot.ddd.task.domain.aggregate.Customer;
-
-public interface CustomerService {
-
-    Customer getCustomerById(Integer customerId);
-}
